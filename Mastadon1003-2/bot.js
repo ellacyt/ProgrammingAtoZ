d527776ea426497637e01dbd4cf7e88291eb50dc
Ella Chung <ellacyt@gmail.com>
2018-10-03 14:11:34 -0400

    update

--- Mastadon1003-2/bot.js
+++ Mastadon1003-2/bot.js
@@ -2,46 +2,49 @@ console.log('The bot is starting');
 
 const config = require('./config.js');
 const Mastodon = require('mastodon-api');
-
 const fs = require('fs');
-
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-
-const cmd = 'processing-java --sketch=`pwd`/squares --run';
-exec(cmd).
-then(stdout, stderr => {
-  console.log(stdout);
-}).
-catch(error => console.error(error));
-
-const stream = fs.createReadStream('squares/output.png');
-
 const M = new Mastodon(config);
 
-const stream = fs.createReadStream('kitten.jpg');
+const cmd = 'processing-java --sketch=`pwd`/squares --run';
 
-const media = {
-  file: stream,
-  description: 'A cute grey kitten holding a blanket found on google image',
-};
+setInterval(toot, 5000);
 
-M.post('media', media).
-then(response => {
-  console.log(response.data.id);
-  // fs.writeFilesSync('response.json',JSON.stringify(response, null, 2));
-  const toot = {
-    status: 'Kitten!',
-    media_ids: [response.data.id]
-  }
-  return M.post('statuses',toot);
-}).
-then(response => {
-  console.log("Success!");
-  console.log(`id: ${response.data.id} at ${response.data.created_at}`);
-  console.log("FINISHED!")
-}).
-catch(error => console.log(error));
+function toot() {
+  exec(cmd).
+  then(response => {
+    console.log(response.stdout);
+    console.log(response.stderr);
+    const stream = fs.createReadStream('squares/output.png');
+    const media = {
+      file: stream,
+      description: 'A random square',
+    };
+    return M.post('media', media);
+  }).
+  then(response => {
+    console.log(response.data.id);
+    // fs.writeFilesSync('response.json',JSON.stringify(response, null, 2));
+    const toot = {
+      status: 'Kitten!',
+      media_ids: [response.data.id]
+    }
+    return M.post('statuses',toot);
+  }).
+  then(response => {
+    console.log("Success!");
+    console.log(`id: ${response.data.id} at ${response.data.created_at}`);
+    console.log("FINISHED!")
+    const stream = fs.createReadStream('kitten.jpg');
+    const media = {
+    file: stream,
+    description: 'A cute grey kitten holding a blanket found on google image',
+  };
+  }).
+  catch(error => console.log(error));
+}
+
 
 // toot();
 
